5e7ed6d3f91dba2fa9e440cffa919083abb78199
transoman <vasiag10@gmail.com>
2019-01-23 15:10:55 +0200

    Layout Project section

--- app/static/js/common.js
+++ app/static/js/common.js
@@ -7,9 +7,32 @@ jQuery(document).ready(function($) {
     $('.header__nav').toggleClass('open');
   });
 
+  // Slider
+  $('.project-modal__slider').each(function(i, el) {
+    var $this = $(this);
+    $this.addClass("project-modal__slider-" + i);
+    $this.parent().find(".swiper-pagination").addClass("swiper-pagination-" + i);
+
+    var pagination = '.swiper-pagination-' + i;
+
+    var slider = new Swiper ('.project-modal__slider-' + i, {
+      pagination: {
+        el: pagination,
+      }
+    });
+
+    $('.project-list__view-detail').click(function() {
+      slider.update();
+      console.log(slider);
+    });
+  });
+
   // Modal
   $('.modal').popup({
     transition: 'all 0.3s',
+    onopen: function() {
+      // slider.update();
+    },
     onclose: function() {
       $(this).find('label.error').remove();
     }
@@ -39,8 +62,47 @@ jQuery(document).ready(function($) {
       element.css('transform', 'translateY(' + imgPos + ')');
     });
   }
-  
+
   simpleParallax(-5, $('.parallax-1'));
+  simpleParallax(-5, $('.parallax-2'));
+  simpleParallax(5, $('.parallax-3'));
+
+  // Show more project
+  var showMoreProject = function() {
+    var list = $('.project-list');
+    var items = list.find('.project-list__item');
+    var countItems = items.length;
+    var maxItems = 5;
+    var arrIndexHidden = [];
+    var btnShow = $('.project__more');
+    var btnTextHide = 'Hide';
+    var btnTextShow = 'Show more';
+
+    if (countItems > maxItems) {
+      items.each(function(i, el) {
+        if (i > 4) {
+          $(this).hide();
+          arrIndexHidden.push(i);
+        }
+      });
+    }
+
+    btnShow.click(function(e) {
+      e.preventDefault();
+      arrIndexHidden.forEach(function(item, i, arrIndexHidden) {
+        items.eq(item).slideToggle();
+      });
+
+      if ($(this).text() == btnTextHide) {
+        $(this).text(btnTextShow);
+      }
+      else {
+        $(this).text(btnTextHide);
+      }
+    });
+  }
+
+  showMoreProject();
 
   var hiddenItemNav = function() {
     var nav = $('.nav-list');
